1d2764c24a4bd579eedc834e9721f4bcf77e1026
MarkOlsonMN <mark.olson.linkedin@gmail.com>
2024-04-14 23:44:04 -0500

    initial assignment save point - added python script stubs, analysis stubs, and cvs data files

---
+++ PyBank/main.py
@@ -0,0 +1,12 @@
+#
+# Module 3 Challenge
+#
+# PyBank Python Script
+#
+# Class: U of M Data Analytics and Visualization Boot Camp , Spring 2024
+# Student: Mark Olson
+# Professor: Thomas Bogue , Assisted by Jordan Tompkins
+# Date: 04/18/2024
+#
+
+print("PyBank - Hello World!")
